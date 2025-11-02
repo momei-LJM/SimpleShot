964228ca8fd2d06a395a463a25b41653aabf043b
momei <2262754355@qq.com>
2025-11-02 11:51:12 +0800

    Initial Commit

---
+++ SimpleShot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleShot
+//
+//  Created by momei on 2025/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleShot/SimpleShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleShotApp.swift
+//  SimpleShot
+//
+//  Created by momei on 2025/11/2.
+//
+
+import SwiftUI
+
+@main
+struct SimpleShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
